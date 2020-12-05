c7f6ae129116422e4d2dde30efdbe122e11cff0f
Nykyta <nikita.mg2@gmail.com>
2020-12-05 10:24:19 +0100

    r

--- thanks.php
+++ thanks.php
@@ -6,9 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Thank you!</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/thankyou.css">
 </head>
 <body>
+    <div class="container one">
+        <div class="detail">Quiz created, thank you!</div>
+        <section>
+            <container>
+
+            </>
+            <div class="container three">
+
+            </div>
+        </section>
+    </div>
 
 
 
