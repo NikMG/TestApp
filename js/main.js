17b91e711497d1ade19e8ffe8b8830510673874c
Nykyta <nikita.mg2@gmail.com>
2020-12-05 03:07:54 +0100

    Valid-Front Done!

--- js/main.js
+++ js/main.js
@@ -5,32 +5,85 @@ let Categ = ValidForm.querySelector('.cat');
 let num = ValidForm.querySelector('.num');
 let fields = ValidForm.querySelectorAll('.field');
 let error = document.querySelectorAll('.error');
-let i;
-let dot = num.value.indexOf(',');
+const pound = {style: 'currency', currency: 'GBP'};
 const regTitle = /^[a-zA-Z0-9_-]{5,50}$/;
-
-
-    const Error = (from, i, elem) => {
+const regNum = /^[0-9.\u00A3]{1,50}$/;
+const validExt = [".jpg", ".png"];
+const Error = (from, i, elem) => {
         console.log("error");
         from.style.borderColor = 'red';
         error[i].innerHTML = elem;
+        return false;
+    };
+
+function ValidTitle(){
+    if (!regTitle.test(Title.value) && Title.value) {
+        Error(Title, 0, "Can't be less than 5 characters!");
+    } else if (!Title.value) {
+        Error(Title, 0, "Please provide a name for your quiz");
+    } else {
+        Error(Title, 0, "");
+        Title.style.borderColor = '#d0d2eb';
     }
+}
+
+function ValidNum(){
+    if(!regNum.test(num.value) | !num.value){
+        num.value = "\u00A3"+"0.00";
+        return false;
+    }else{
+        num.value = new Intl.NumberFormat('en-IN', pound).format(num.value);
+    }
+}
+
+function ValidCat(){
+    if(!Categ.value){
+        Error(Categ, 2, "Please select a category");
+    }else{
+        Error(Categ, 2, "");
+        Categ.style.borderColor = '#d0d2eb';
+    }
+}
+
+function ValidFile(){
+    let FileName = Logo.value;
+    if (FileName.length > 0){
+        let blnValid = false;
+        for(let j = 0; j < validExt.length; j++){
+            let CurExt = validExt[j];
+            if (FileName.substr(FileName.length - CurExt.length, CurExt.length).toLowerCase() == CurExt.toLowerCase()){
+                blnValid = true;
+                break;
+            }
+        }
+        if(!blnValid){
+            alert("Wrong ex");
+            Logo.value = "";
+            return false;
+        }
+    }
+}
 
 
     Title.addEventListener('blur', (event) => {
         event.preventDefault();
-        if (!regTitle.test(Title.value) && Title.value) {
-            Error(Title, 0, "Can't be less than 5 characters!");
-            return false;
-        } else if (!Title.value) {
-            Error(Title, 0, "Please provide elem");
-            return false;
-        } else {
-            Error(Title, 0, "");
-            Title.style.borderColor = '#d0d2eb';
-        }
+        ValidTitle();
     });
 
+    num.addEventListener('blur', (event) => {
+        event.preventDefault();
+        ValidNum();
+    });
+
+    Categ.addEventListener('blur', (event) => {
+        event.preventDefault();
+        ValidCat();
+    });
+
+    Logo.addEventListener('blur', (event) => {
+        event.preventDefault();
+        ValidFile();
+    });
 
 
 
